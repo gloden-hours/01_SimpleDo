99642912ee07256fac933b68b731aff427723c8e
DESKTOP-L81L9DC\blind <gold46548@gmail.com>
2024-02-05 19:27:42 +0900

    24.02.05 MySQL 연결

---
+++ src/main/java/com/goldenhour/SimpleDo/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.goldenhour.SimpleDo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("home")
+    public String home(Model model){
+        //model.addAttribute("data", "hello!!");
+        return "home";
+    }
+}
